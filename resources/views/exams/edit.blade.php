328aa7e68944dc9dbd6ef338481c738acd5de861
Ramez <ramez.shaban.948@gmail.com>
2023-12-15 22:32:32 +0300

    last las

--- resources/views/exams/edit.blade.php
+++ resources/views/exams/edit.blade.php
@@ -1,20 +1,27 @@
 @extends('exams.layout')
 @section('content')
- 
+
 <div class="card">
-  <div class="card-header">Exam Page</div>
-  <div class="card-body">
-      
-      <form action="{{ url('exam/' .$exams->id) }}" method="post">
-        {!! csrf_field() !!}
-        @method("PATCH")
-        <input type="hidden" name="id" id="id" value="{{$exams->id}}" id="id" />
-        <label>Name</label></br>
-        <input type="text" name="name" id="name" value="{{$exams->name}}" class="form-control"></br>
-        <input type="submit" value="Update" class="btn btn-success"></br>
-    </form>
-   
-  </div>
+    <div class="card-header">Exam Page</div>
+    <div class="card-body">
+        <form action="{{ url('exam/' . $exams->id) }}" method="post">
+            {!! csrf_field() !!}
+            @method("PATCH")
+            <input type="hidden" name="id" value="{{ $exams->id }}" />
+            
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" value="{{ $exams->name }}" class="form-control">
+            </div>
+            
+            <div class="form-group">
+                <label for="description">Description:</label>
+                <textarea name="description" id="description" class="form-control">{{ $exams->description }}</textarea>
+            </div>
+            
+            <input type="submit" value="Update" class="btn btn-success">
+        </form>
+    </div>
 </div>
- 
+
 @stop
